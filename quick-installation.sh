971542b0fdfb45c78474ce5c0642a45314416464
Rizwan Fairuz Mamduh <rizwanfairuzmamduh29@gmail.com>
2024-11-24 17:46:46 +0700

    Update quick-installation.sh

--- quick-installation.sh
+++ quick-installation.sh
@@ -4,6 +4,7 @@
 echo -e "\033[0;32mUpdating and Installing dependencies...\033[0m"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y ca-certificates apt-transport-https zlib1g-dev software-properties-common libncurses5-dev libgdbm-dev libnss3-dev curl git wget make jq build-essential pkg-config lsb-release libssl-dev libreadline-dev libffi-dev gcc screen unzip lz4 expect
+clear 
 
 # Install Docker
 echo -e "\033[0;32mAdd repository and Installing Docker...\033[0m"
@@ -18,6 +19,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin -y 
+clear
 
 # Install Python
 echo -e "\033[0;32mInstall Python3 and Pip3...\033[0m"
@@ -26,6 +28,7 @@ sudo apt update
 sudo apt install -y python3.12 python3.12-venv python3.10-venv
 # install pip
 sudo curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
+clear
 
 # Install Nodejs
 echo -e "\033[0;32mInstall Nodejs via nvm-list...\033[0m"
@@ -35,6 +38,7 @@ nvm list-remote
 
 echo -e "\033[0;32mSelect your Nodejs version : \033[0m" NODEJS_USER
 nvm use $NODEJS_USER
+clear
 
 # Install PHP
 echo -e "\033[0;32mInstall php and php extension...\033[0m"
@@ -42,9 +46,9 @@ sudo apt update -y && sudo apt upgrade -y
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update -y
 sudo apt-get install php8.3 php8.3-mysql php8.3-imap php8.3-ldap php8.3-xml php8.3-curl php8.3-mbstring php8.3-zip -y
+clear
 
 # Install mariadb
 echo -e "\033[0;32mInstall MariaDB Database...\033[0m"
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install mariadb-server -y
-sudo mysql_secure_installation
